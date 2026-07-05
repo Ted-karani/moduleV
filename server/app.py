cedef75ec58b99f8338b38f5d88571fb2d9999e7
Ted karani <ted.karani@student.moringaschool.com>
2026-07-05 12:50:04 +0300

    added crud routes, works okay with postman

--- server/app.py
+++ server/app.py
@@ -9,5 +9,59 @@ inventory = [
     {"id": 3, "name": "Orange Juice", "brand": "Tropicana", "price": 4.99, "stock": 20}
 ]
 
+
+@app.route("/inventory", methods=["GET"])
+def get_inventory():
+    return jsonify(inventory), 200
+
+
+@app.route("/inventory/<int:id>", methods=["GET"])
+def get_item(id):
+    item = next((i for i in inventory if i["id"] == id), None)
+    if item:
+        return jsonify(item), 200
+    return jsonify({"error": "Item isnt found"}), 404
+
+
+@app.route("/inventory", methods=["POST"])
+def add_item():
+    data = request.get_json()
+    new_id = len(inventory) + 1
+    new_item = {
+        "id": new_id,
+        "name": data["name"],
+        "brand": data["brand"],
+        "price": data["price"],
+        "stock": data["stock"]
+    }
+    inventory.append(new_item)
+    return jsonify(new_item), 201
+
+
+@app.route("/inventory/<int:id>", methods=["PATCH"])
+def update_item(id):
+    item = next((i for i in inventory if i["id"] == id), None)
+    if item:
+        data = request.get_json()
+        if "name" in data:
+            item["name"] = data["name"]
+        if "brand" in data:
+            item["brand"] = data["brand"]
+        if "price" in data:
+            item["price"] = data["price"]
+        if "stock" in data:
+            item["stock"] = data["stock"]
+        return jsonify(item), 200
+    return jsonify({"error": "Item isnt found"}), 404
+
+
+@app.route("/inventory/<int:id>", methods=["DELETE"])
+def delete_item(id):
+    item = next((i for i in inventory if i["id"] == id), None)
+    if item:
+        inventory.remove(item)
+        return jsonify({"message": "Item its deleted"}), 200
+    return jsonify({"error": "Item isnt found"}), 404
+
 if __name__ == '__main__':
     app.run(debug=True)
